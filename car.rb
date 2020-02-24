23b506ccbc9986eb9a9387cc63b8fb3472c7cdbb
Igor Petrovski <ipetrovski3@gmail.com>
2020-02-24 12:54:53 +0100

    Making car to drive

---
+++ car.rb
@@ -0,0 +1,37 @@
+class MyCar
+  
+  def initialize(model, engine, year, color)
+    @model = model
+    @engine = engine
+    @year = year
+    @color = color
+    @current_speed = 0
+    puts "I am driving: #{model} #{engine} #{year} #{color}"
+  end
+
+  def speed_up(kmh)
+    @current_speed += kmh
+    puts "We are going to pass that vechile in front of us, by driving #{kmh} Km/H!"
+  end
+
+  def brake(kmh)
+    @current_speed -= kmh
+    puts "Ohh s*it, they are cops ahead we must brake and maintain our speed by #{kmh} Km/H!"
+  end
+
+  def speed
+    puts "We are now driving #{@current_speed} Km/H. We avoided that speeding ticket!"
+  end
+
+  def shut_off
+    @current_speed = 0
+    puts "We have reached our destination! Turn off the ignition!"
+  end
+end
+
+mycar = MyCar.new("Peugeot 308SW", "1.6HDI", "2008", "Silver")
+puts "Start your engine!"
+mycar.speed_up(180)
+mycar.brake(100)
+mycar.speed
+mycar.shut_off
