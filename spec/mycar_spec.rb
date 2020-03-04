b2c83a9b772cbfcd4c2d68adb8c171e3ca604f38
Igor Petrovski <ipetrovski3@gmail.com>
2020-03-04 10:06:10 +0100

    added new speed_up test

--- spec/mycar_spec.rb
+++ spec/mycar_spec.rb
@@ -1,14 +1,17 @@
 RSpec.describe Car::MyCar do
 
-  peugeot = Car::MyCar.new("Peugeot", 2008, "Silver")
-
   it "Create new instance" do
-    
+    peugeot = Car::MyCar.new("Peugeot", 2008, "Silver")
     expect(peugeot).to be_instance_of(Car::MyCar)
   end
 
   it "It has starting speed defined to 0" do
-    # peugeot = Car::MyCar.new("Peugeot", 2008, "Silver")
+    peugeot = Car::MyCar.new("Peugeot", 2008, "Silver")
     expect(peugeot.current_speed).to eq(0)
+  end
+
+  it "It speeds up" do
+    peugeot = Car::MyCar.new("Peugeot", 2008, "Silver")
+    expect(peugeot.speed_up).to be > 0
   end
 end
